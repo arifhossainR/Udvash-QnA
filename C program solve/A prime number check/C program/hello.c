7c6f1d8670434d8bce01a3ea41f2a90abe8fb9b4
arifhossainR <arifcse012@gmail.com>
2026-01-14 02:09:31 +0600

    QNA

--- C program solve/A prime number check/C program/hello.c
+++ C program solve/A prime number check/C program/hello.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int num, i, count = 0;
+    int num, i, status = 0;
     printf("Enter a number: ");
     scanf("%d", &num);
 
     for(i = 2; i < num; i++){
         if(num%i == 0){
-            count = 1;
+            status = 1;
             break;
         }
     }
 
-    if(count == 1){
+    if(status == 1){
         printf("%d is not a prime number.", num);
     }
     else{
--- C program solve/রোল নাম্বার অনুযায়ী সেকশন/hello.c
+++ C program solve/রোল নাম্বার অনুযায়ী সেকশন/hello.c
@@ -2,5 +2,21 @@
 
 int main()
 {
+    int roll;
+    printf("Enter your roll number: ");
+    scanf("%d", &roll);
+
+    if(roll >= 1 && roll <= 30){
+        printf("Group A.");
+    }
+    else if(roll >= 31 && roll <= 60) {
+        printf("Group B.");
+    }
+    else if(roll >= 61 && roll <= 100) {
+        printf("Group C.");
+    }
+    else {
+        printf("Invalid roll number.");
+    }
     return 0;
 }
